674ade6b96fbd918384c69eff0a5a1dc6b1faec4
radityaM <78536660+radityaM@users.noreply.github.com>
2021-06-13 17:52:34 +0700

    Add files via upload

---
+++ Hero.js
@@ -0,0 +1,30 @@
+class Hero {
+  constructor(x,y,r)
+	{
+		var options = { 
+			density: 1, 
+			frictionAir:0.5
+		};
+		this.x=x;
+		this.y=y;
+		this.r=r;
+		this.image=loadImage("superhero1.png");
+		this.body=Bodies.circle(this.x, this.y, (this.r)/2, options);
+		World.add(world, this.body);
+
+	}
+	
+	display()
+	{
+			
+			var heroPos=this.body.position;		
+			push()
+			translate(heroPos.x, heroPos.y-60);
+			rectMode(CENTER)
+			fill(255,0,255)
+			imageMode(CENTER);
+			image(this.image, 0,0,this.r+200, this.r)
+			pop()
+			
+	}
+}
